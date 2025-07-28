f1d57857444abd2eb9e92ec0d8425c41dd7a08bf
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-07-29 03:40:28 +0500

    add dashboard screen

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 
+// import './custom.js';
+
+import 'remixicon/fonts/remixicon.css';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/css/custom.css',
+                'resources/js/app.js'
+                // 'resources/js/custom.js',
             ],
             refresh: true,
         }),
